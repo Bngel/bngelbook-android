94c80780243dceb1fb213a1c709db6d6c73f1da3
bngel <bngel1024@gmail.com>
2021-12-25 21:36:15 +0800

    feat: add view of verification

--- app/src/main/java/cn/bngel/bngelbook/activity/LoginActivity.kt
+++ app/src/main/java/cn/bngel/bngelbook/activity/LoginActivity.kt
@@ -2,34 +2,49 @@ package cn.bngel.bngelbook.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key.Companion.Sleep
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.edit
 import cn.bngel.bngelbook.network.api.UserApi
 import cn.bngel.bngelbook.ui.theme.BngelbookTheme
 import cn.bngel.bngelbook.ui.widget.UiWidget.Dialog_Loading
+import kotlinx.coroutines.delay
+import kotlin.concurrent.thread
 
 class LoginActivity : BaseActivity() {
 
     private val WAY_SMS = 0
     private val WAY_PASSWORD = 1
+    private val WAY_VERIFICATION = 2
     private val loginWay = mutableStateOf(WAY_SMS)
     private val loading = mutableStateOf(false)
+    private var userPhone = ""
+    private val SMS_CLOCK = 60
+    private val smsClock = mutableStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,62 +60,82 @@ class LoginActivity : BaseActivity() {
     @Composable
     private fun LoginPage() {
         if (loading.value)
-            Dialog_Loading{loading.value = false}
-        Box(contentAlignment = Alignment.Center, modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(
-                brush = Brush.verticalGradient(
-                    listOf(
-                        Color(0xff66ccff),
-                        Color(0x6666ccff)
+            Dialog_Loading { loading.value = false }
+        Box(
+            contentAlignment = Alignment.Center, modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            Color(0xff66ccff),
+                            Color(0x6666ccff)
+                        )
                     )
                 )
-            )
-            .padding(start = 40.dp, end = 40.dp)) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                .padding(start = 40.dp, end = 40.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .background(
                         color = Color(0xFFFFFFFF),
                         shape = RoundedCornerShape(20.dp)
                     )
-                    .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 30.dp)) {
-                Text(text = "Welcome", fontSize = 22.sp, modifier = Modifier
-                    .padding(top = 10.dp, bottom = 30.dp)
-                    .align(Alignment.CenterHorizontally))
+                    .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 30.dp)
+            ) {
+                Text(
+                    text = "Welcome", fontSize = 22.sp, modifier = Modifier
+                        .padding(top = 10.dp, bottom = 30.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
                 when (loginWay.value) {
                     WAY_PASSWORD -> LoginPasswordWay()
                     WAY_SMS -> LoginSmsWay()
+                    WAY_VERIFICATION -> VerificationWay()
                 }
             }
         }
     }
 
     @Composable
-    private fun LoginSmsWay() {
-        var userPhone by remember {
-            mutableStateOf("")
+    private fun VerificationWay() {
+        VerificationCodeField(digits = 4) { text,focused ->
+            SimpleVerificationCodeItem(text = text, focused = focused)
         }
+    }
+
+    @Composable
+    private fun LoginSmsWay() {
         TextField(modifier = Modifier.padding(10.dp),
             value = userPhone,
-            label = { Text(text = "Tel", fontSize = 12.sp) } ,
+            label = { Text(text = "Tel", fontSize = 12.sp) },
             singleLine = true,
             placeholder = { Text(text = "请输入手机号", fontSize = 16.sp) },
             onValueChange = {
                 userPhone = it
             })
         Spacer(modifier = Modifier.height(30.dp))
-        Button(modifier = Modifier.padding(top = 20.dp),
-            colors = ButtonDefaults.buttonColors(contentColor = Color(0xFFFFFFFF),
+        Button(enabled = smsClock.value == 0,
+            modifier = Modifier.padding(top = 20.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFFFFFFFF),
                 backgroundColor = Color(0xFF66CCFF)
             ),
             shape = RoundedCornerShape(10.dp),
             onClick = {
-                loginWay.value = WAY_PASSWORD
+                smsClock.value = SMS_CLOCK
+                thread {
+                    while (smsClock.value > 0) {
+                        smsClock.value -= 1
+                        Thread.sleep(1000)
+                    }
+                }
+                loginWay.value = WAY_VERIFICATION
             }) {
-            Text(text = "获取验证码")
+            Text(text =  if (smsClock.value == 0) "获取验证码" else "${smsClock.value}秒后重试")
         }
-        Row(modifier = Modifier.padding(10.dp)) {
+        Row(modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 20.dp, bottom = 10.dp)) {
             Text(text = "密码登录", textAlign = TextAlign.Start, fontSize = 13.sp,
                 modifier = Modifier
                     .weight(1F)
@@ -114,7 +149,7 @@ class LoginActivity : BaseActivity() {
     }
 
     @Composable
-    private fun LoginPasswordWay(){
+    private fun LoginPasswordWay() {
         var userAccount by remember {
             mutableStateOf("")
         }
@@ -123,7 +158,7 @@ class LoginActivity : BaseActivity() {
         }
         TextField(modifier = Modifier.padding(10.dp),
             value = userAccount,
-            label = { Text(text = "Account", fontSize = 12.sp) } ,
+            label = { Text(text = "Account", fontSize = 12.sp) },
             singleLine = true,
             placeholder = { Text(text = "请输入手机号", fontSize = 16.sp) },
             onValueChange = {
@@ -131,7 +166,7 @@ class LoginActivity : BaseActivity() {
             })
         TextField(modifier = Modifier.padding(10.dp),
             value = userPassword,
-            label = { Text(text = "Password", fontSize = 12.sp) } ,
+            label = { Text(text = "Password", fontSize = 12.sp) },
             singleLine = true,
             placeholder = { Text(text = "请输入密码", fontSize = 16.sp) },
             visualTransformation = PasswordVisualTransformation(),
@@ -139,7 +174,8 @@ class LoginActivity : BaseActivity() {
                 userPassword = it
             })
         Button(modifier = Modifier.padding(top = 20.dp),
-            colors = ButtonDefaults.buttonColors(contentColor = Color(0xFFFFFFFF),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xFFFFFFFF),
                 backgroundColor = Color(0xFF66CCFF)
             ),
             shape = RoundedCornerShape(10.dp),
@@ -160,32 +196,38 @@ class LoginActivity : BaseActivity() {
                                     putString("account", account)
                                     putString("password", password)
                                 }
-                                Toast.makeText(this@LoginActivity, "登录成功", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this@LoginActivity, "登录成功", Toast.LENGTH_SHORT)
+                                    .show()
                                 finish()
                             }
                             400 -> {
                                 intent.putExtra("loginState", false)
                                 this@LoginActivity.setResult(RESULT_OK, intent)
-                                Toast.makeText(this@LoginActivity, "用户名或密码错误", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this@LoginActivity, "用户名或密码错误", Toast.LENGTH_SHORT)
+                                    .show()
                             }
                             else -> {
                                 intent.putExtra("loginState", false)
                                 this@LoginActivity.setResult(RESULT_OK, intent)
-                                Toast.makeText(this@LoginActivity, "网络状态异常, 请稍后再试", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "网络状态异常, 请稍后再试",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
-                    }
-                    else {
+                    } else {
                         intent.putExtra("loginState", false)
                         this@LoginActivity.setResult(RESULT_OK, intent)
-                        Toast.makeText(this@LoginActivity, "网络状态异常, 请稍后再试", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@LoginActivity, "网络状态异常, 请稍后再试", Toast.LENGTH_SHORT)
+                            .show()
                     }
                     loading.value = false
                 }
             }) {
             Text(text = "登录")
         }
-        Row(modifier = Modifier.padding(10.dp)) {
+        Row(modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 20.dp, bottom = 10.dp)) {
             Text(text = "验证码登录", textAlign = TextAlign.Start, fontSize = 13.sp,
                 modifier = Modifier
                     .weight(1F)
@@ -195,9 +237,77 @@ class LoginActivity : BaseActivity() {
                     ) {
                         loginWay.value = WAY_SMS
                     })
-            Text(text = "找回密码", textAlign = TextAlign.End, fontSize = 13.sp,
-                modifier = Modifier.weight(1F))
+            Text(
+                text = "找回密码", textAlign = TextAlign.End, fontSize = 13.sp,
+                modifier = Modifier.weight(1F)
+            )
         }
     }
 
+
+    /**
+     * @param text 文本内容
+     * @param focused 是否高亮当前输入框
+     */
+    @Composable
+    fun SimpleVerificationCodeItem(text: String, focused: Boolean) {
+        val borderColor = if (focused) Color.Gray else Color(0xeeeeeeee)
+        Box(
+            modifier = Modifier
+                .border(1.dp, borderColor)
+                .size(50.dp, 50.dp), contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 1
+            )
+        }
+
+    }
+
+
+    @Composable
+    fun VerificationCodeField(
+        digits: Int,
+        horizontalMargin: Dp = 10.dp,
+        inputCallback: (content: String) -> Unit = {},
+        itemScope: @Composable (text: String, focused: Boolean) -> Unit
+    ) {
+        var content by remember { mutableStateOf("") }
+        Box {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                repeat(digits) {
+                    if (it != 0) {
+                        Spacer(modifier = Modifier.width(horizontalMargin))
+                    }
+                    val text = content.getOrNull(it)?.toString() ?: ""
+                    val focused = it == content.length
+                    itemScope(text, focused)
+                }
+
+            }
+            BasicTextField(
+                value = content, onValueChange = {
+                    if (it.length <= digits)
+                        content = it
+                    if (it.length == digits)
+                        inputCallback(it)
+                }, modifier = Modifier
+                    .drawWithContent {}
+                    .matchParentSize()
+            )
+        }
+    }
+
+    override fun onBackPressed() {
+        if (loginWay.value == WAY_VERIFICATION)
+            loginWay.value = WAY_SMS
+        else
+            super.onBackPressed()
+    }
 }
